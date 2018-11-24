dc08071f36579f444a9716d7afb3011ec35d2ecc
Runner85sx <debran@live.de>
2018-11-24 21:16:49 +0100

    msm8974-common: gestures: Set LOCAL_PRIVATE_PLATFORM_APIS
    This change sets LOCAL_SDK_VERSION for all packages where
    this is possible without breaking the build, and
    LOCAL_PRIVATE_PLATFORM_APIS := true otherwise.
    
    Setting one of these two will be made required soon, and this
    is a change in preparation for that. Not setting LOCAL_SDK_VERSION
    makes the app implicitly depend on the bootclasspath, which is
    often not required. This change effectively makes depending on
    private apis opt-in rather than opt-out.
    
    Bug: 73535841
    Change-Id: Iabb0556dc1c80c7fc7f6c76d61d5e441b03cdce0

--- dotcase/Android.mk
+++ dotcase/Android.mk
@@ -10,6 +10,8 @@ LOCAL_SRC_FILES := $(call all-java-files-under, src)
 LOCAL_PACKAGE_NAME := Dotcase
 LOCAL_CERTIFICATE := platform
 
+LOCAL_PRIVATE_PLATFORM_APIS := true
+
 LOCAL_PROGUARD_FLAG_FILES := proguard.flags
 
 include $(BUILD_PACKAGE)
